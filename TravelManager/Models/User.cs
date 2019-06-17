93b8987c5cd8427fc06c170f18fa93dc6cdfcf34
Demid Zhukov <1demidz3@gmail.com>
2019-06-17 03:08:48 +0300

    Not used User's Password property - removed

--- TravelManager/Models/User.cs
+++ TravelManager/Models/User.cs
@@ -9,7 +9,7 @@ namespace TravelManager.Models
     {
         public long UserId { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
+        //public string Password { get; set; }
         public string Email { get; set; }
         public string IdentityId { get; set; }
         public long CurrencyId { get; set; }
